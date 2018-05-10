de0b607217af4aa3565d9792fb22e57340109fb0
enkaypeter <enkaypeter@gmail.com>
2018-05-10 08:11:02 +0100

    changin cron runtime interval

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('route:call /scrape')->everyFiveMinutes();
+        $schedule->command('route:call /scrape')->hourlyAt(480);	
     }
 
     /**
